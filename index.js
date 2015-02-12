b7644fd58bfa9946accdcb643154375f1d8ec3af
Lance Pollard <lancejpollard@gmail.com>
2015-02-12 19:07:34 +0000

    remove mouseover event listeners on hide

--- index.js
+++ index.js
@@ -412,6 +412,8 @@ Tip.prototype.hide = function(ms){
 Tip.prototype.remove = function(){
   this.winEvents.unbind('resize', 'reposition');
   this.winEvents.unbind('scroll', 'reposition');
+  this.handleEvents.unbind('mouseover');
+  this.handleEvents.unbind('mouseout');
   this.emit('hide');
 
   var parent = this.el.parentNode;
